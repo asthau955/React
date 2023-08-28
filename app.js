18781d61dd6fb7a1ca3908060cdd8daf7146963e
astha <asthaupadhyay955@gmail.com>
2023-08-28 21:56:57 +0530

    --commit for component

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
+
+// react element
 const heading = React.createElement("h1", {id: "heading"}, "Hello World!")
 
 // nested html tags
@@ -51,5 +53,39 @@ const parent3 = React.createElement("div", { id: "parent" }, [
    */
 
 
+  /**
+   * JSX - an extension to js - HTML like syntax // code is transpiled before it reaches to JS engine
+   * traspiled - conversion into browser understandable code
+   * transpilation is done using Parcel (parcel needs babel for this operation)
+   * html tag class === jsx className
+   * jsx attributes are in camel case ex: tabIndex
+   */
+
+const jsxHeading = (
+  <h1 id="heading" className="heading" tabIndex="5">
+    Hello World JSX!
+  </h1>
+); 
+
+
+/**
+ * React components - 1) class based components 2 )functional components
+ * function which returns react element or JSX code is a functional component
+ */
+
+
+const HeadingComponent = () =>{ return <h1>Functional Component</h1> }; 
+
+/**
+ * or
+ * const headingComponent = () =>{ <h1>Heading Component</h1> }; 
+ */
+
+
+// component composition - component in side another component 
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent3)
+
+root.render(<HeadingComponent />)
+
+// root.render(jsxHeading)
